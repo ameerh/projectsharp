af19ddb3ea8ffbd4374788916891005de28978ae
ameer <ameer@novatoresols.com>
2014-05-28 21:47:11 +0500

    up to date

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,6 +8,7 @@ class EventsController < ApplicationController
   # GET /events.json
   def index
     @date = Date.today
+    @events = Event.where("DATE(event_datetime) = ? AND league = 'MLB'",@date)
     # @events = Event.where("(league = ? OR league = ?) AND event_datetime >= ?", "NBA", "MLB",  Time.now.to_datetime).order('event_datetime ASC').limit(5)
     @all_games = Event.where("(league = ? OR league = ?) AND event_datetime >= ?", "NBA", "MLB",  Time.now.to_datetime).order('event_datetime ASC').limit(5)
     @all_nba = Event.where("league = ? AND event_datetime >= ?", "NBA", Time.now.to_datetime).order('event_datetime ASC').paginate(:page => params[:page], :per_page => 5)
