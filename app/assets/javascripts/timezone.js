95c8032a1aad80c92940c3c9e7e6bb5ec7f02ac3
ameer <ameer@novatoresols.com>
2014-05-29 22:44:27 +0500

    main table changes

--- app/assets/javascripts/timezone.js
+++ app/assets/javascripts/timezone.js
@@ -2,4 +2,8 @@ function set_time_zone_offset() {
     var current_time = new Date();
     $.cookie('time_zone', current_time.getTimezoneOffset());
 }
+if($.cookie('time_zone') == null)
+{
+	location.reload()
+}
 set_time_zone_offset();
